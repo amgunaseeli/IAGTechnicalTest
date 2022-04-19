50e24eb35c3df23331f1f49bdf4280b9b9b48de7
Gunaseeli <am.gunaseeli@gmail.com>
2022-04-19 22:41:13 +1200

    IAG test backend completed API

--- Backend/VehicleSummary.Api/Services/VehicleSummary/VehicleSummaryService.cs
+++ Backend/VehicleSummary.Api/Services/VehicleSummary/VehicleSummaryService.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Flurl;
-using Flurl.Http;
+using System.Diagnostics;
+using System.Linq;
+using RestSharp;
+using VehicleSummary.Api.Model;
 
 
 namespace VehicleSummary.Api.Services.VehicleSummary
@@ -12,32 +14,55 @@ namespace VehicleSummary.Api.Services.VehicleSummary
     {
         Task<VehicleSummaryResponse> GetSummaryByMake(string make);
     }
-    
     public class VehicleSummaryService : IVehicleSummaryService
     {
+        private static string BASE_URL = "https://api.iag.co.nz/vehicles/vehicletypes/makes/";
+        private static string HEADER_SUBSCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
+        private static string HEADER_SUBSCRIPTION_KEY_VALUE = "72ec78fb999e43be8dbdac94d7236cae";
+
+        public RestClient RestClient { get; set; }
+        public VehicleSummaryService()
+        {
+            RestClient = new RestClient(BASE_URL)
+            .AddDefaultHeader(HEADER_SUBSCRIPTION_KEY, HEADER_SUBSCRIPTION_KEY_VALUE);
+        }
+
         public async Task<VehicleSummaryResponse> GetSummaryByMake(string make)
         {
-            throw new NotImplementedException();
-        }
+            VehicleSummaryResponse response = new VehicleSummaryResponse();
+            response.Make = make;
+            response.Models = new List<VehicleSummaryModels>();
+            List<string> models = await getModels(make);
+            foreach (string model in models)
+            {
+                VehicleSummaryModels vehicleSummaryModel = await getVehicleModelYearsSummary(make, model);
+                response.Models.Add(vehicleSummaryModel);
+            }
 
-        
-        /*
-         Here's a small helper. We're using Flurl for http requests. (Change it if you wish)
-         https://flurl.dev/
-            
-        async Task<List<string>> getModels(string make)
-        {   
-            var modelsUrl = "https://api.iag.co.nz/vehicles/vehicletypes/makes/Lotus/models?api-version=v1";
-
-            var response = await modelsUrl
-                .WithHeader("Ocp-Apim-Subscription-Key", "72ec78fb999e43be8dbdac94d7236cae")
-                .GetJsonAsync<List<string>>();
-                
             return response;
-              
         }
-        */
-        
-        
+        private async Task<List<string>> getModels(string make)
+        {
+            string modelsUrl = "{make}/models";
+            RestRequest request = new RestRequest(modelsUrl, Method.Get)
+            .AddUrlSegment("make", make)
+            .AddQueryParameter("api-version", "v1");
+            var response = await RestClient.GetAsync<List<string>>(request);
+            return response;
+        }
+        private async Task<VehicleSummaryModels> getVehicleModelYearsSummary(string make, string model)
+        {
+            VehicleSummaryModels vehicleSummaryModel = new VehicleSummaryModels();
+            string yearsAvailableUrl = "{make}/models/{model}/years";
+            RestRequest request = new RestRequest(yearsAvailableUrl, Method.Get)
+            .AddUrlSegment("make", make)
+            .AddUrlSegment("model", model)
+            .AddQueryParameter("api-version", "v1"); ;
+            var response = await RestClient.GetAsync<List<int>>(request);
+            vehicleSummaryModel.Name = model;
+            vehicleSummaryModel.YearsAvailable = response.Count();
+            return vehicleSummaryModel;
+        }
+
     }
 }
